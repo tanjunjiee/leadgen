cd467401b56af84eea3b8d81e1f737151c68246e
tanjunjiee <tanjunjiee@gmail.com>
2026-05-28 14:18:56 +0800

    Update db.js
    fix db path

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, '../data/leads.db');
+const DB_PATH = process.env.DB_PATH || '/tmp/leads.db';
 
 let db;
 
